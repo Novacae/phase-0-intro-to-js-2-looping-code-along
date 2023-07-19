616c46e84b7840d02263e2b1e2c3bb4a5eaa9658
aman githinji <amangithinji09@gmail.com>
2023-07-19 21:40:37 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solutions in this file
+// Code your solutions in this fil
+
+function writeCards() {
+let writeCards =  (["Guadalupe", "Ollie", "Aki"],"suprise") 
+for (let i = 0; i < writeCards.length; i++) {
+     return  writeCards = [
+        "Thank you, Guadalupe, for the wonderful surprise gift!",
+        "Thank you, Ollie, for the wonderful surprise gift!",
+        "Thank you, Aki, for the wonderful surprise gift!",
+     ]
+        
+    }
+}
+
+function countDown() {
+  var i = '';
+   while( i >= 0) {
+   }
+   console.log(i--);
+}
